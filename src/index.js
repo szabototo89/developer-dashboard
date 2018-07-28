50a2bc4bc5d88048c38a6a92e2560ec01b569d34
Tamás Szabó <szabototo89@hotmail.com>
2018-07-28 20:45:15 +0200

    Add icons to dashboard menu component

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { DashboardMenu } from "./dashboard/dashboard-menu/dashboard-menu";
 import { MenuItem } from "./common/menu-item/menu-item";
+import { Icon } from "./common/icon/icon";
 
 ReactDOM.render(
   <DashboardMenu onSelectionChanged={console.log}>
-    <MenuItem name="project">Project</MenuItem>
-    <MenuItem name="build">Build</MenuItem>
-    <MenuItem name="git">Git</MenuItem>
-    <MenuItem name="about">About</MenuItem>
+    <MenuItem name="project">
+      <Icon type="project-diagram" />
+      Project
+    </MenuItem>
+    <MenuItem name="build">
+      <Icon type="code" />
+      Build
+    </MenuItem>
+    <MenuItem name="git">
+      <Icon type="code-branch" />
+      Git
+    </MenuItem>
   </DashboardMenu>,
   document.getElementById("application")
 );
