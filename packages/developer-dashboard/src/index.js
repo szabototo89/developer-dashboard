e1cad995b726c39f181409a38914bbab3a1bd4a9
Tamás Szabó <szabototo89@hotmail.com>
2018-07-29 20:38:31 +0200

    Almost finishing integration

--- packages/developer-dashboard/src/index.js
+++ packages/developer-dashboard/src/index.js
@@ -2,6 +2,6 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Application } from "./application/application";
 
-import "babel-polyfill"
+import "babel-polyfill";
 
 ReactDOM.render(<Application />, document.getElementById("application"));
