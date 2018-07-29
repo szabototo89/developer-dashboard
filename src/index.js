ad1b314f1dde05f30672842f4c393ecb20f1817d
Tamás Szabó <szabototo89@hotmail.com>
2018-07-29 20:16:51 +0200

    Extract Application component from index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { DashboardMenu } from "./dashboard/dashboard-menu/dashboard-menu";
-import { DashboardMenuItem } from "./dashboard/dashboard-menu-item/dashboard-menu-item";
-import { Icon } from "./common/icon/icon";
-import { DashboardUser } from "./dashboard/dashboard-user/dashboard-user";
+import { Application } from "./application/application";
 
-ReactDOM.render(
-  <React.Fragment>
-    <DashboardUser fullName="John Doe" />
+import "babel-polyfill"
 
-    <DashboardMenu onSelectionChanged={console.log}>
-      <DashboardMenuItem name="project">
-        <Icon type="project-diagram" />
-        Project
-      </DashboardMenuItem>
-      <DashboardMenuItem name="build">
-        <Icon type="code" />
-        Build
-      </DashboardMenuItem>
-      <DashboardMenuItem name="git">
-        <Icon type="code-branch" />
-        Git
-      </DashboardMenuItem>
-      <DashboardMenuItem name="my-todo-list">
-        <Icon type="list-ul" />
-        My Todo List
-      </DashboardMenuItem>
-    </DashboardMenu>
-  </React.Fragment>,
-  document.getElementById("application")
-);
+ReactDOM.render(<Application />, document.getElementById("application"));
