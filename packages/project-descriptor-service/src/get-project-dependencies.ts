0fcd65fbd0747563fb5cd2039a16b0b2bdaad325
Tamás Szabó <szabototo89@hotmail.com>
2018-07-30 21:57:12 +0200

    add getProjectDependencies() function

--- packages/project-descriptor-service/src/get-project-dependencies.ts
+++ packages/project-descriptor-service/src/get-project-dependencies.ts
@@ -1,5 +1,33 @@
-import { NpmPackageDescriptor } from "./read-npm-package-descriptor";
+import { NpmPackageDescriptor, ProjectName, ProjectVersion } from "./read-npm-package-descriptor";
+import { map, pipe } from "ramda";
 
-export function getProjectDependencies(packageDescriptor: NpmPackageDescriptor) {
+export function getProjectDependencies(
+  packageDescriptor: NpmPackageDescriptor
+) {
+  const { devDependencies, dependencies } = packageDescriptor;
 
+  const asDependency = (isDevelopmentOnly: boolean) => {
+    return (value: [ProjectName, ProjectVersion]) => {
+      const [projectName, projectVersion] = value;
+      return {
+        name: projectName,
+        specifiedVersion: projectVersion,
+        actualVersion: null,
+        isDevelopmentOnly
+      };
+    };
+  };
+
+  const convertToDependencies = (isDevelopmentOnly: boolean) =>
+    pipe(
+      Object.entries,
+      map<[ProjectName, ProjectVersion], ReturnType<ReturnType<typeof asDependency>>>(
+        asDependency(isDevelopmentOnly)
+      )
+    );
+
+  return [
+    ...convertToDependencies(false)(dependencies),
+    ...convertToDependencies(true)(devDependencies)
+  ];
 }
